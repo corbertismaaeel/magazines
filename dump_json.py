6630d3effedebcde6233b3d64c02ef92470c7c4a
corbertismaaeel <corbertismaaeel4036@gmail.com>
2021-02-25 16:15:13 +0800

    update protocol

--- dump_json.py
+++ dump_json.py
@@ -6,13 +6,19 @@ import json
 from json import JSONEncoder
 
 
+# RES_BASE_URL = 'https://gitee.com/corbertismaaeel/magazines/raw/master/economist/20210220/cover.jpeg'
+RES_BASE_URL = 'https://gitee.com/corbertismaaeel/magazines/raw/master/{}/{}/{}'
+
+
 class MagazineIssue(object):
 
     mag_name = ""
-    file_name = ""
+    title = ""
     pub_date = 0
-    cover_name = ""
+    cover = ""
     formats = None
+    source = 'gitee'
+    download_url = ''
 
     def __init__(self, name, p_date, formats):
         self.mag_name = name
@@ -23,7 +29,7 @@ class MagazineIssue(object):
         self.formats.append(book_format)
 
     def __str__(self):
-        return "name : %s, file_name : %s, pub_date : %d" % (self.mag_name, self.file_name, self.pub_date)
+        return "name : %s, title : %s, pub_date : %d" % (self.mag_name, self.title, self.pub_date)
 
 
 class MagEncoder(JSONEncoder):
@@ -43,7 +49,7 @@ def main(root_dir):
         for issue_date in issues:
             if os.path.isfile(issue_date):
                 continue
-            a_mag_issue = process_mag_issue(mag.get('name'), issue_date, mag_dir + os.path.sep + issue_date)
+            a_mag_issue = process_mag_issue(mag, issue_date, mag_dir + os.path.sep + issue_date)
             if a_mag_issue is not None:
                 all_mag_issues.append(a_mag_issue)
     # 按照发布日期排序
@@ -59,21 +65,26 @@ def dump_json(root_dir, all_mag_issues):
             jfile.write(json_content)
 
 
-def process_mag_issue(mag_name, pub_date, issue_dir):
+def process_mag_issue(mag, pub_date, issue_dir):
     try:
+        mag_dir = mag.get('dir')
+        mag_name = mag.get('name')
         mag_issue = MagazineIssue(mag_name, int(pub_date), formats=[])
+        mag_issue.source = 'gitee'
         book_files = os.listdir(issue_dir)
         for f in book_files:
             if f.lower().endswith(".epub"):
                 mag_issue.add_format("epub")
-                mag_issue.file_name = os.path.splitext(f)[0]
+                mag_issue.title = os.path.splitext(f)[0]
+                mag_issue.download_url = RES_BASE_URL.format(mag_dir, pub_date, mag_issue.title)
 
             if f.lower().endswith(".pdf"):
                 mag_issue.add_format("pdf")
-                mag_issue.file_name = os.path.splitext(f)[0]
+                mag_issue.title = os.path.splitext(f)[0]
+                mag_issue.download_url = RES_BASE_URL.format(mag_dir, pub_date, mag_issue.title)
 
             if f.lower() == "cover.jpg" or f.lower() == "cover.jpeg" or f.lower() == "cover.png":
-                mag_issue.cover_name = f
+                mag_issue.cover = RES_BASE_URL.format(mag_dir, pub_date, f)
         return mag_issue
     except Exception as e:
         print("process_mag_issue error " + str(e))
